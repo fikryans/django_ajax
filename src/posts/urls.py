20509ee4a1aa189bc82968a9f70fad14eda51c2f
fikryans <fikriansyah.hq@gmail.com>
2021-03-31 13:45:15 +0700

    adding like unlike posts

--- src/posts/urls.py
+++ src/posts/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('', views.post_list_and_create, name='main-board'),
     path('hello-world/', views.helloWorld, name='hello-world'),
     path('data/<int:num_posts>/', views.load_post_data_view, name='load-post'),
+    path('like-unlike/', views.like_unlike_post_view, name="like-unlike"),
 
 ]
 
