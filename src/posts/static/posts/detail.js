f2c4172483ec12f0f3b906a9117b66168fc261c9
fikryans <fikriansyah.hq@gmail.com>
2021-04-01 21:54:27 +0700

    adding modal update and delete post

--- src/posts/static/posts/detail.js
+++ src/posts/static/posts/detail.js
@@ -1,6 +1,9 @@
 console.log("Detail posts");
 
 const backBtn = document.getElementById("back-btn")
+const updateBtn = document.getElementById("update-btn") 
+const deletBtn = document.getElementById("delete-btn")
+const postBox = document.getElementById("post-box")
 const url = window.location.href + "data/"
 const spinnerBox = document.getElementById("spinner-box")
 
@@ -13,6 +16,27 @@ $.ajax({
     url: url,
     success: function (response) {
         console.log(response);
+        const data = response.data
+
+        if(data.logged_in !== data.author){
+            console.log('different');
+        } else {
+            console.log('the same');
+            updateBtn.classList.remove('not-visible')
+            deletBtn.classList.remove('not-visible')
+            
+        }
+        const titleEl = document.createElement('h3')
+        titleEl.setAttribute('class', 'mt-1')
+
+        const bodyEl = document.createElement('p')
+        bodyEl.setAttribute('class', 'mt-1')
+
+        titleEl.textContent =data.title
+        bodyEl.textContent = data.body
+
+        postBox.appendChild(titleEl)
+        postBox.appendChild(bodyEl)
         spinnerBox.classList.add('not-visible')
     },
     error: function (error) {
