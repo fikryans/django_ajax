f1fab5d212b75032f7afcb4dccbe23b6c418fdcb
fikryans <fikriansyah.hq@gmail.com>
2021-04-01 21:17:21 +0700

    working on the post detail page part 1

--- src/posts/views.py
+++ src/posts/views.py
@@ -61,6 +61,18 @@ def load_post_data_view(request, num_posts):
         }
         return JsonResponse (context, safe=False)
 
+def post_detail_data_view(request, pk):
+    obj = Post.objects.get(pk=pk)
+    data = {
+        'id': obj.id,
+        'title': obj.title,
+        'body': obj.body,
+        'author': obj.author.user.username,
+        'logged_in': request.user.username,
+    }
+
+    return JsonResponse({'data':data})
+
 def like_unlike_post_view(request):
     if request.is_ajax():
         pk = request.POST.get('pk')
